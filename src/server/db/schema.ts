76a2d518c8c5b77774fe570edf9595be6b1fbf00
Mantas Imbrasas <mantasimbrasas@gmail.com>
2024-05-13 19:58:20 +0300

    create database schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,6 +4,8 @@
 import { sql } from "drizzle-orm";
 import {
   index,
+  integer,
+  pgEnum,
   pgTableCreator,
   serial,
   timestamp,
@@ -18,17 +20,44 @@ import {
  */
 export const createTable = pgTableCreator((name) => `nextjs-raffle_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+const roleEnum = pgEnum("role", ["user", "admin", "influencer"]);
+
+export const users = createTable("user", {
+  id: serial("id").primaryKey(),
+  nickname: varchar("nickname").notNull(),
+  email: varchar("email").notNull().unique(),
+  image: varchar("image").notNull(),
+  role: roleEnum("role").notNull().default("user"),
+  createdAt: timestamp("created_at").default(sql`now()`),
+  ethereumAddress: varchar("ethereum_address").notNull().unique(),
+  // ^ could do a check to see if the address is valid, but drizzleORM doesn't support that yet
+});
+
+export const raffles = createTable("raffle", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  chain: varchar("chain").notNull(),
+  currency: varchar("currency").notNull(),
+  ticketPrice: varchar("ticket_price").notNull(),
+  prize: varchar("prize").notNull(),
+  image: varchar("image").notNull(),
+  startDate: timestamp("start_date").notNull(),
+  participantCount: integer("participant_count").notNull().default(0),
+  maxParticipants: integer("max_participants").notNull(),
+  // below are filled after the raffle ends
+  winnerId: integer("winner_id").references(() => users.id),
+  endDate: timestamp("end_date"),
+  txHash: varchar("tx_hash"),
+  videoOfDraw: varchar("video"),
+});
+
+export const tickets = createTable("ticket", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id),
+  userWalletAddress: varchar("user_wallet_address").notNull(),
+  raffleId: integer("raffle_id")
+    .references(() => raffles.id)
+    .notNull(),
+  txHash: varchar("tx_hash").notNull(),
+  createdAt: timestamp("created_at").default(sql`now()`),
+});
